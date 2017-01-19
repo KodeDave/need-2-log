5337c7fe483007e8a3a8545941b76a7978976b9e
Quattrocchi Davide <davide.quattrocchi@outlook.com>
2017-01-19 20:08:23 +0100

    created solution

---
+++ N2L-Need_2_Log/N2L-Need_2_Log/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N2L_Need_2_Log
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
