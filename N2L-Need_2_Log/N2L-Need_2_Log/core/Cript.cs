497877bf114df8fc6eb052f4c2c2813413ec40f9
Quattrocchi Davide <davide.quattrocchi@outlook.com>
2017-01-19 20:25:10 +0100

    created some initial file

---
+++ N2L-Need_2_Log/N2L-Need_2_Log/core/Cript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2L_Need_2_Log.core
+{
+    class Cript
+    {
+    }
+}
